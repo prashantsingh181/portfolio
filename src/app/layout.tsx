51a4be43989568d6fd1810e087493161a11d3fab
Prashant Singh <prashant.singh181@gmail.com>
2025-10-22 12:11:59 +0530

    fix: clarity code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,14 +106,13 @@ export default async function RootLayout({
         />
 
         <script
-          id="clarity"
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "ttr0v37ur9");`,
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ttr0v37ur9");`,
           }}
         />
       </head>
